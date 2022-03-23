804665b7bda3657a0e3135def1ad3be4605a9197
G <Prabagaran_G@Dellteam.com>
2022-03-23 22:47:39 +0530

    changes

--- client/src/app/doctors/profile-settings/profile-settings.component.ts
+++ client/src/app/doctors/profile-settings/profile-settings.component.ts
@@ -1,12 +1,81 @@
 import { Component, OnInit } from "@angular/core";
-
+import { Router, ActivatedRoute } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControlOptions,
+} from "@angular/forms";
+import { AuthService } from "../../core";
 @Component({
   selector: "profile-settings",
   templateUrl: "./profile-settings.component.html",
   styleUrls: ["./profile-settings.component.scss"],
 })
 export class ProfileSettingsComponent implements OnInit {
-  constructor() {}
+  panelOpenState = false;
+  preliminaryForm: FormGroup;
+  userData;
+  submitted = false;
+  step = 0;
 
-  ngOnInit() {}
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.userData = this.authService.currentUserValue;
+    this.preliminaryForm = this.formBuilder.group({
+      firstName: [
+        this.userData.firstName,
+        [Validators.required, Validators.pattern("^[a-zA-Z '-]+$")],
+      ],
+      lastName: [
+        this.userData.lastName,
+        [Validators.required, Validators.pattern("^[a-zA-Z '-]+$")],
+      ],
+      email: [
+        this.userData.email,
+        [Validators.required, Validators.email, Validators.minLength(5)],
+      ],
+      mobile: [
+        this.userData.mobile,
+        [
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+          Validators.minLength(10),
+        ],
+      ],
+      smobile: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("^[0-9]*$"),
+          Validators.minLength(10),
+        ],
+      ],
+      role: ["Doctor", []],
+    });
+  }
+  get f() {
+    return this.preliminaryForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    //console.log(this.preliminaryForm.value);
+  }
 }
